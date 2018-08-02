390a4b392d049042cfbe6a14873c1e7a1dabd359
Dalton Lima <daltonvarussa@gmail.com>
2018-08-02 19:21:26 -0300

    add  1st working scene

---
+++ Assets/Scripts/InteractionHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionHelper : MonoBehaviour
+{
+
+    public Material MaterialInGaze;
+    private Material _oldMaterial;
+    private GameObject _objectInFocus;
+
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+	{
+	    var ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+	    RaycastHit raycastInfo;
+
+	    if (Physics.Raycast(ray, out raycastInfo))
+	    {
+	        var hitObject = raycastInfo.transform.gameObject;
+	        if (hitObject == _objectInFocus) { return; }
+	        var renderer = hitObject.GetComponent<Renderer>();
+
+	        if (renderer == null) { return; }
+
+	        _oldMaterial = renderer.material;
+	        renderer.material = MaterialInGaze;
+	        _objectInFocus = hitObject;
+	    }
+	    else
+	    {
+	        if (_objectInFocus == null) return;
+
+	        var renderer = _objectInFocus.GetComponent<Renderer>();
+	        renderer.material = _oldMaterial;
+	        _objectInFocus = null;
+	    }
+	}
+}
