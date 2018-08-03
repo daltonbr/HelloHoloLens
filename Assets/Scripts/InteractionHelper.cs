e3b587664f53f647d7401c7cf3a0735f7aebfd67
Dalton Lima <daltonvarussa@gmail.com>
2018-08-03 02:04:29 -0300

    Remove obsolete event

--- Assets/Scripts/InteractionHelper.cs
+++ Assets/Scripts/InteractionHelper.cs
@@ -16,21 +16,28 @@ public class InteractionHelper : MonoBehaviour
     {
 		GestureRecognizer gestureRecognizer = new GestureRecognizer();
         gestureRecognizer.SetRecognizableGestures(GestureSettings.Tap);
-        // TODO change this deprecated delegate
-        gestureRecognizer.TappedEvent += GestureRecognizer_TappedEvent;
-        gestureRecognizer.StartCapturingGestures();
+        // Obsolete Event
+        //gestureRecognizer.TappedEvent += GestureRecognizer_TappedEvent;        
+        Action<TappedEventArgs> tapAction = OnTappedEvent;        
+        gestureRecognizer.Tapped += tapAction;
+        gestureRecognizer.StartCapturingGestures();     
     }
 
-    private void GestureRecognizer_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
-    {
-        //throw new System.NotImplementedException();
+    //private void GestureRecognizer_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
+    //{
+    //    //throw new System.NotImplementedException();
+    //    if (_objectInFocus == null) { return; }        
+    //    _objectInFocus.SendMessage("DoDrop");
+    //}
+
+    private void OnTappedEvent(TappedEventArgs tappedEventArgs)
+    {    
         if (_objectInFocus == null) { return; }
-        Debug.Log("Source: " + source.ToString() + " tapCount: " + tapCount + " headRay direction: " + headRay.direction);
         _objectInFocus.SendMessage("DoDrop");
     }
 
-	
-	void Update ()
+
+    void Update ()
 	{
 	    var ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
 	    RaycastHit raycastInfo;
