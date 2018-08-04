27a80a516d39911693292cf4b9744d19b0faf7f7
Dalton Lima <daltonvarussa@gmail.com>
2018-08-03 21:02:14 -0300

    Speech recognition

--- Assets/Scripts/InteractionHelper.cs
+++ Assets/Scripts/InteractionHelper.cs
@@ -2,14 +2,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Windows.Speech;
 using UnityEngine.XR.WSA.Input;
 
 public class InteractionHelper : MonoBehaviour
 {
-
     public Material MaterialInGaze;
     private Material _oldMaterial;
     private GameObject _objectInFocus;
+    public GameObject ObjectToReset;
 
 
     void Start ()
@@ -20,15 +21,22 @@ public class InteractionHelper : MonoBehaviour
         //gestureRecognizer.TappedEvent += GestureRecognizer_TappedEvent;        
         Action<TappedEventArgs> tapAction = OnTappedEvent;        
         gestureRecognizer.Tapped += tapAction;
-        gestureRecognizer.StartCapturingGestures();     
+        gestureRecognizer.StartCapturingGestures();
+
+        KeywordRecognizer keywordRecognizer = new KeywordRecognizer(new [] {"Stop"});
+        keywordRecognizer.OnPhraseRecognized += KeywordRecognizer_OnPhraseRecognized;
     }
 
-    //private void GestureRecognizer_TappedEvent(InteractionSourceKind source, int tapCount, Ray headRay)
-    //{
-    //    //throw new System.NotImplementedException();
-    //    if (_objectInFocus == null) { return; }        
-    //    _objectInFocus.SendMessage("DoDrop");
-    //}
+    private void KeywordRecognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        if (ObjectToReset == null) { return; }
+
+        // Reset our Object to initial position
+        var rb = ObjectToReset.GetComponent<Rigidbody>();
+        rb.useGravity = false;
+        rb.velocity = Vector3.zero;
+        ObjectToReset.transform.position = new Vector3(0f, 0f, 2f);
+    }
 
     private void OnTappedEvent(TappedEventArgs tappedEventArgs)
     {    
