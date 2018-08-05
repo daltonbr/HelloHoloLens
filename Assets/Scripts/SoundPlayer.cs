fedff0ff96239c72ebf97178269eee20cb61b56e
Dalton Lima <daltonvarussa@gmail.com>
2018-08-04 22:21:29 -0300

    Add Spatial Audio to the ball

---
+++ Assets/Scripts/SoundPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayer : MonoBehaviour
+{
+    public void OnCollisionEnter(Collision collision)
+    {
+        var audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+    }
+
+}
